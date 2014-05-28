61539967bba24a65c27adc569bd7de98c8735f6d
Justin T. Wagner <jwag789@gmail.com>
2014-05-28 16:34:46 -0400

    added my_solution

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,10 +1,11 @@
 // I paired [by myself, with:] on this challenge.
 
-// __________________________________________
+// By Myself
 // Write your code below.
-
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", "Eddard", "Daenerys", "Mary"];
 
 
 
@@ -12,13 +13,14 @@
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
-
+// I thought that this was a good excercise to go through to understand Test Driven Development.
+// Test Driven Development is a technique that always helps me. It will typically tell you the error
+// that is happening if there is one, and it is a good way to fix through bugs to solve a problem.
+// I've worked with TDD's before and they have always been helpful. It's very interesting working
+// "backwards" and it helped me figure out some things. I did not face any problems in this
+// excercise or have many questions, since the coding in question was rather simple. The concept of 
+// TDD was once new to me though but is now solidified in my head. I enjoyed doing all of this and
+// nothing was tedious, because I thought it was a great way to dive into TDD.
 
 // __________________________________________
 // Test Code:  Do not alter code below this line.
