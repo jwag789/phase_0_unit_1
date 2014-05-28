2dfe050787a49d345c18a1537b453f79be996560
Justin T. Wagner <jwag789@gmail.com>
2014-05-27 23:13:04 -0400

    Added my_solution.js and my_solution.md (challenge 1 and 2, week 3)

--- week_3/1_git_started/my_solution.md
+++ week_3/1_git_started/my_solution.md
@@ -3,53 +3,79 @@ Define the following commands and describe how they work/what they do.
 
 
 #### add
-<!-- Your defnition here -->
+This is used to start tracking a file, or stage a change of a file, from your working directory, to your index. Add is a good term for this, since you are telling git to now track these files.
 
 #### branch
-<!-- Your defnition here -->
+This allows you to list the local branches you have, but when you add a name in parenthesis, a new branch is created with that name. This allows you to create your own branch of a project that  other people are working on, so you can make your own changes to it without affecting everyones work, and can be merged later on.
 
 #### checkout
-<!-- Your defnition here -->
+This tells git that you want to switch the branch that you are working on. To switch to a specific branch, you can type in 'git checkout branchname', with the name of your branch instead in place of branchname.
 
 #### clone
-<!-- Your defnition here -->
+By running git clone, you can essentially clone or copy a project locally, so that you can look at and edit the code yourself. This will give you the entire history of the project.
 
 #### commit
-<!-- Your defnition here -->
+After staging what you want to snapshot with git add, this is used to "save" the snapshot, as I like to put it. When making a commit, you create a saved version of your project that you can pull up at any time. It is required to leave a message with each commit to find your versions easier.
 
 #### fetch
-<!-- Your defnition here -->
+This will synchronize you with another repository, adding any data that you do not have locally, without merging the changes. It allows you to merge when you are ready.
 
 #### log
-<!-- Your defnition here -->
+This will display the list of commits on the branch you are in, with their commit messages displayed as well.
 
 #### merge
-<!-- Your defnition here -->
+This will merge a branch into your cuurrent branch, and creates a new commit if there are no conflicts.
 
 #### pull
-<!-- Your defnition here -->
+This is essentially the combination of the commands 'git fetch' and 'git merge'. It basically does what fetch does, except it merges changes right away. It is generally a better idea to use fetch so you can decide when to merge.
 
 #### push
-<!-- Your defnition here -->
+This will "push" (or upload, as I like to think of it) your changes to to a remote repository. It is used to share your commits, say on Github.
 
 #### reset
-<!-- Your defnition here -->
+This will be useful when you track a file for staging that you decide that you actually do not want to be in your snapshot when you commit. This will revert the staging area to what was there before modification. It can also be used to undo your last commit, putting the files back in stage.
 
 #### rm
-<!-- Your defnition here -->
+This will simply remove an entry from the staging area. The file will not be in the staging area at all and will not be included in the next commit, and is essentially deleted.
 
 #### status
+This will show you the status of your staging area, in comparison to the code in your working directory. It will tell you what files are untracked, what files are modified, and what files are staged.
 
 
 ## Release 4: Git Workflow
 
 - Push files to a remote repository
+
+git add file.ext
+git commit -m 'message here'
+git push
+
 - Fetch changes
+
+git remote add upstream https://yourURLhere.git
+git fetch upstream
+(This will store the upstream's master branch on your local branch)
+git merge upstream/master
+(Merges your master branch into the upstream branch)
+
+
 - Commit locally
+
+git add file.ext
+git commit -m 'message here'
+
 
 ## Release 5: Errors you encountered
 1. Error
 2. Solution
 3. Cause (explain the error)
 
+I have not come across any errors... just yet.
+
 ## Release 6: Reflection
+
+While learning all about git and Github, I was faced with a little confusion. There were so many commands and some of them seemed so similar, and I always got afraid that I would make errors that would hurt me. But when I read more, I started to get a really good feel of how git works. By going through a few commits through the command line, I was really able to get a feel of at least the most basic commands. I didn't have many specific questions, more just apprehension that for some reason something wouldn't work right. But as I tried a few times, git recorded all of my commits, added and removed the files I wanted, and worked well so far. It seems like a really amazing tool to update and share your project easily while saving every version that you commit. There are still some things that I wonder how they work exactly such as the differences between git fetch and git pull, and when I might want to use git pull instead. Otherwise I learned many commands and shortcuts that are helpful. I feel confident with the learning competencies of this section, but I still have to look at references once in a while. I enjoyed testing out commands in the terminal and having my changes made. If I found anything tedious it was reading an extensive amount about version history, as that seems almost self explanatory to me, while the specific commands are more complex.  
+
+These two resources were great: http://www.siteground.com/tutorials/git/commands.htm and http://gitref.org/
+
+
--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -4,13 +4,229 @@
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
- 
- 
- 
- 
- 
- 
- 
- 
+// Level 1 - Rescue Mission
+
+// move down x 2
+// move right
+// move up x 2
+// move right x 2
+// move down
+// Attack!
+
+this.moveDown();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+// Level 2 - Grab the Mushroom
+
+// move up
+// move right
+// move left
+// move up
+// Attack!
+
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+
+this.attackNearbyEnemy();
+
+// Level 3 - Drink Me
+
+// Attack!
+// move right
+// move down
+// move up
+// move right
+// Attack!
+
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+
+// Level 4 - Taunt the Guards
+
+// move right
+// bust door
+
+// move right
+// say hey (taunt)
+// move left x 2
+// say attack
+
+// move right
+// say follow
+// move right x 2
+// move up
+// move right
+// say hey (taunt)
+// move down
+// move right
+// move up
+// move right
+
+this.moveRight();
+this.bustDownDoor();
+
+this.moveRight();
+this.say("Hey there!");
+this.moveLeft();
+this.moveLeft();
+this.say("Attack!");
+
+this.moveRight();
+this.say("Follow me.");
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.say("Hey there!");
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+
+// Level 5 - It's a Trap
+
+// move down x 2
+// say hey (taunt)
+// move up x 2
+
+this.moveDown();
+this.moveDown();
+this.say("Hello there Ogre!");
+this.moveUp();
+this.moveUp();
+
+// Level 6 - Break the Prison
+
+// if william, true
+// if krogg, false
+// if lucas, true
+// if brack or gort, false
+// if robert or marcus, true
+// if grul'thock, false
+// if gordon, true
+
+if(name === "William")
+    return true;
+if(name === "Krogg")
+    return false;
+if(name === "Lucas")
+    return true;
+if(name === "Brack" || name  === "Gort")
+    return false;
+if(name === "Robert" || name == "Marcus")
+    return true;
+if(name === "Grul'Thock")
+    return false;
+if(name === "Gordon")
+    return true;
+
+// Level 7 - Taunt
+
+// say hey (taunt)
+// say more (taunt) x 3
+
+this.say("Hey!");
+this.say("Ogre, I'm talking to you!");
+this.say("That's right, lughead! You!");
+this.say("YOU!!!");
+
+// Level 8 - Cowardly Taunt
+
+// move to (50,16)
+// move to (63,20)
+// move to (70,10) (hiding)
+// say (I can lure them here)
+
+// move (60,42)
+
+// say hey (taunt)
+
+// move to (70,10) (hiding)
+
+this.moveXY(50, 16);
+this.moveXY(63, 20);
+this.moveXY(70, 10);
+this.say("I can lure them in here.");
+
+this.moveXY(60, 42);
+
+this.say("Hey y'all.")
+
+this.moveXY(70, 10);
+
+// Level 9 - Commanding Followers
+
+// move to (49,66)
+// move to (60,63)
+// move to (75,63)
+// say message to friends, "follow"
+// move to (61,41)
+// say message to friends, "attack"
+
+this.moveXY(49, 66);
+this.moveXY(60, 63);
+this.moveXY(75, 63);
+this.say("Hail, friends!");
+this.say("Follow me, troops!");
+this.moveXY(61, 41);
+this.say("On my mark... 1...2...3...ATTACK!");
+
+// Level 10 - Mobile Artillery
+
+// move to (30,26) (close to ogres)
+// attack (46,5)
+
+// move to (54,37) (close to other ogres)
+// attack (48,66) (ogres and big ogre hit 1)
+// attack (53,51) (big ogre hit 2)
+// attack (67,55) (ogres and big ogre hit 1)
+// attack (59,44) (big ogre hit 2)
+
+this.moveXY(30, 26);
+this.attackXY(46, 5);
+
+this.moveXY(54, 37);
+this.attackXY(48,66);
+this.attackXY(53,51);
+this.attackXY(67,55);
+this.attackXY(59,44);
+
+// Release 3 - Answer the following questions
+
+// What is "this" referring to?
+
+//The keyword "this" in Javascript is essentially referring to the owner of the function that is being executed. Or for example, if you set a variable to something, and call "this" on the variable in a function, it will return the value of the variable.
+
+// What does the "()" do in JavaScript?
+
+// The parenthesis in JavaScript are used at the end of a function name, because it allows for the entry of a parameter. It may be left empty if your function does not require a parameter, but is still necessary syntax.
+
+// The semicolons in JavaScript are used to seperate different statements. If the statement is followed by a line break, it can be omitted, but it is useful for seperating statements on the same line.
+
+
 // Reflection:
 // Write your reflection here.
+
+// This was an interesting activity to complete. I love video games so that resonated
+// with me. It was also useful to sort of see how functions can play out in a visual sense,
+// and it was definitely useful to practice writing in psuedocode and then translating
+// it to code. Other than that I thought that this excercise was a little too simple even,
+// as I felt the psuedocode I was writing was exactly the same as the real code, and
+// other than writing in psuedocode, not much was being really taught. The excercises
+// pretty much consisted of moving around a couple of function calls. I definitely enjoyed
+// reading through each of the functions and how they worked however, and it gave me a good
+// sense of how "this" is used, and a few other syntax points. A good resource for this section
+// that I found was: http://www.codecademy.com/blog/78-your-guide-to-semicolons-in-javascript
