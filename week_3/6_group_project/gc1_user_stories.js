69c7ca88c2eee11e41fe863e40ef0b42ea0e94e9
Justin T. Wagner <jwag789@gmail.com>
2014-05-28 16:34:46 -0400

    added user stories

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,13 @@
+// My role in the group is: Person 1 - Translate tests into user stories
+
+// Here is my part of the challenge:
+
+// Test 1 - I want to make a program so that I can find the sum of any group of numbers
+// Test 2 - I want to make a program able to sum up an odd amount of numbers
+// Test 3 - I want to make a program able to sum up an even amount of numbers
+// Test 4 - I want to make a program that will find the average (mean) of any group of numbers
+// Test 5 - I want to make a program to be able to find the average (mean) of an odd amount of numbers
+// Test 6 - I want to make a program to be able to find the average (mean) of an even amount of numbers
+// Test 7 - I want to make a program that will find the median of any group of numbers
+// Test 8 - I want to make a program able to find the median of an odd amount of numbers
+// Test 9 - I want to make a program able to find the median of an even amount of numbers
