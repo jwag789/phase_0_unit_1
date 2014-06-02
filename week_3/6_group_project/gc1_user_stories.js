da1373b4b5f540e5529cef5ca95fb7655c5e5571
Justin T. Wagner <jwag789@gmail.com>
2014-06-01 23:27:00 -0400

    Updated group project

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -11,3 +11,11 @@
 // Test 7 - I want to make a program named median that will find the median of any group of numbers
 // Test 8 - I want to make a program named median able to find the median of an odd amount of numbers
 // Test 9 - I want to make a program named median able to find the median of an even amount of numbers
+
+// This was an interesting experience and it was good to work as a group for practice. It was definitely interesting,
+// and was slightly hard scheduling everything between four people, but it was nice working together on a common
+// goal. At first I wasn't really sure what to write for the user storis, but as I did a little research I understood
+// better, and understood the benefits. As person 1, I did release 4 and tested the final code, and all of the tests
+// passed. It was cool seeing the process from user stories to psuedocode to initial code to refactored code. It made
+// the process more understandable to me in terms of breaking down and understanding what to do, especially when
+// working with multiple people.
